c2e8a3b92f246659d826ca802d7d62cc40562bf6
Karun Shrestha <kars.kit@gmail.com>
2020-08-27 00:14:49 +0545

    First commit

---
+++ app/src/main/java/com/halo/splashloginmotionlayout/Activity_Splash_Login.java
@@ -0,0 +1,14 @@
+package com.halo.splashloginmotionlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Splash_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+    }
+}
